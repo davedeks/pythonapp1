3e2e90b641cc80a2ddf524f2c9667dc16a12e7a3
David Deko <davedeksy@yahoo.com>
2023-09-14 14:26:15 -0500

    Deploy 3

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
-def main():
-    return "Welcome!"
-
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
-    
+def helloworld():
+    return "Hello World!"
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=8080)
+    app.run()
